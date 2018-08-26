843fc4b729e74e01ca1e19e667ea3ce8ff042c0d
Moya <moyaccercchi@hotmail.de>
2018-08-26 22:09:06 +0200

    Improve versioning

--- src/com/asofterspace/webengine/Main.java
+++ src/com/asofterspace/webengine/Main.java
@@ -3,13 +3,14 @@ package com.asofterspace.webengine;
 import javax.swing.SwingUtilities;
 
 import com.asofterspace.toolbox.configuration.ConfigFile;
+import com.asofterspace.toolbox.Utils;
 import com.asofterspace.toolbox.web.JSON;
 
 public class Main {
 	
-	public static String PROGRAM_TITLE = "WebEngine";
-	public static String VERSION_NUMBER = "0.0.0.2";
-	public static String VERSION_DATE = "4. October 2017 - 26. August 2018";
+	public final static String PROGRAM_TITLE = "WebEngine";
+	public final static String VERSION_NUMBER = "0.0.0.2(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
+	public final static String VERSION_DATE = "4. October 2017 - 26. August 2018";
 	
 	public static void main(String[] args) {
 		
